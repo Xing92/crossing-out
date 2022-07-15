e0757cc189c9c6b15a42233c0dc82ed670e0bc76
Pawel Ciazynski <pawel.ciazynski@ericsson.com>
2022-07-15 09:50:23 +0200

    initial commit and db connection

---
+++ crossing-out-server/src/main/java/com/xing/main/CrossingOutApplication.java
@@ -0,0 +1,13 @@
+package com.xing.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrossingOutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrossingOutApplication.class, args);
+		System.out.println("Xing:started");
+	}
+}
---
+++ crossing-out-server/src/main/java/com/xing/main/model/User.java
@@ -0,0 +1,79 @@
+package com.xing.main.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+@Entity
+@Table(name = "user")
+@JsonIdentityInfo(generator = ObjectIdGenerators.IntSequenceGenerator.class, property = "@id")
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(unique = true)
+	private String username;
+	private String password;
+//	@OneToOne(cascade = CascadeType.ALL)
+//	private Kingdom kingdom;
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+	private Set<Role> roles = new HashSet<>();
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+//	public Kingdom getKingdom() {
+//		return kingdom;
+//	}
+//
+//	public void setKingdom(Kingdom kingdom) {
+//		this.kingdom = kingdom;
+//	}
+
+	public Set<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Set<Role> roles) {
+		this.roles = roles;
+	}
+
+	public void addRole(Role role) {
+		this.roles.add(role);
+	}
+
+}
